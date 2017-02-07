25cb53ea6f72f9fc24809edf0970887c8da0eb2e
Geoffrey Pascoe <gmp@robots.ox.ac.uk>
2017-02-07 11:13:37 +0000

    add ollie to results auth

--- src/admin.py
+++ src/admin.py
@@ -27,8 +27,8 @@ ERROR_ALREADY_EXISTS = 1281
 ERROR_NO_EMAIL = 1282
 ERROR_INVALID_PHONE = 1283
 
-mens_results_viewers = ['pascoeg@gmail.com']
-womens_results_viewers = ['pascoeg@gmail.com']
+mens_results_viewers = ['pascoeg@gmail.com', 'o.w.bartlett@gmail.com']
+womens_results_viewers = ['pascoeg@gmail.com', 'o.w.bartlett@gmail.com']
 
 class AddPlayerHandler(webapp2.RequestHandler):
   def get(self):
